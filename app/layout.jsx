0f1b0b7bd05f3f8bbccdee42e4e8519030609487
gcahyono <gcahyono19@gmail.com>
2025-05-21 09:09:55 +0700

    memperbaiki metadata tag

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,14 +2,16 @@
 
 import { SessionProvider } from "next-auth/react";
 
-export const metadata = {
-  title: "Page",
-  description: "Login Google di NextJS 14.2.28 dengan NextAuth.js",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Page Title</title>
+        <meta
+          name="Page"
+          content="Login Google di NextJS 14.2.28 dengan NextAuth.js"
+        />
+      </head>
       <body>
         <SessionProvider>{children}</SessionProvider>
       </body>
