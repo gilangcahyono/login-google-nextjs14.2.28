940267dd9dada79c01980b880b2dd41f94daa3cf
gcahyono <gcahyono19@gmail.com>
2025-05-21 09:00:30 +0700

    menambahkan metadata tag

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,11 @@
 
 import { SessionProvider } from "next-auth/react";
 
+export const metadata = {
+  title: "Page",
+  description: "Login Google di NextJS 14.2.28 dengan NextAuth.js",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
